9398ddf15b46d3df4d064bfc580d3305314beae8
Marco <marco.fini22@gmail.com>
2023-05-22 10:04:54 -0300

    flip-camera with gray scale

--- capture-video-camera/flip-camera.py
+++ capture-video-camera/flip-camera.py
@@ -1,11 +1,24 @@
+import numpy as np
 import cv2 as cv
 
 cap = cv.VideoCapture(0)
 
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
+# Define the codec and create VideoWriter object
+fourcc = cv.VideoWriter_fourcc(*'XVID')
+out = cv.VideoWriter('test.avi', fourcc, 30.0, (1920,  1080))
 
-while True:
-    rec, frame = cap.read()
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        print("camera cannot start")
+        break
+    frame = cv.flip(frame, 0)
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
+
+    # write the flipped frame
+    out.write(gray)
+    cv.imshow('xd', gray)
+    
+    if cv.waitKey(1) == ord('q'):
+        break
